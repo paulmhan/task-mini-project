13182aec9ecc0dbf97ec109d9805c582122b8a66
Paul Han <paul.myung.han@gmail.com>
2020-10-20 13:13:02 -0700

    refactor tasks imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SignInComponent } from "./sign-in/sign-in.component";
 import { SignUpComponent } from "./sign-up/sign-up.component";
+import { TasksComponent } from "./tasks/tasks.component";
 
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'sign-up', component: SignUpComponent},
+  { path: 'tasks', component: TasksComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,19 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header/header.component';
-import { TasksComponent } from './tasks/tasks.component';
-import { TaskItemComponent } from './tasks/task-item/task-item.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
+import { TasksComponent } from './tasks/tasks.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    TasksComponent,
-    TaskItemComponent,
     SignInComponent,
     SignUpComponent,
-    TaskDetailComponent
+    TasksComponent,
   ],
   imports: [
     BrowserModule,
