115ee3157ed7bbc684c5aed90bd5323c49ac964f
Paul Han <paul.myung.han@gmail.com>
2020-10-23 10:39:02 -0700

    added imports

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-// require('dotenv').config();
+require('dotenv').config();
 const Koa = require("koa");
+const Router = require("koa-router");
 
 
 const app = new Koa();
+const router = new Router();
+
+
 
 
 // Create the server
-app.listen(process.env.PORT || 3000);
-console.info(`${process.version} listening on port ${process.env.PORT || 3000}`);
+app.listen(process.env.PORT || 3001);
+console.info(`${process.version} listening on port ${process.env.PORT || 3001}`);
 
