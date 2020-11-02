e6eb293e1a914eae3360ccea9592d6991395038d
Paul Han <paul.myung.han@gmail.com>
2020-11-02 15:23:05 -0800

    added log out functionality

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  onLogOut(){
+    this.auth.logout();
+    this.router.navigate(['/sign-in']);
   }
 
 }
