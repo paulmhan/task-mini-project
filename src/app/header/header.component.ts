f223c86ac7d32f2497e165546c10c26e4f5b3c9b
Paul Han <paul.myung.han@gmail.com>
2020-11-04 15:25:10 -0800

    navbar updates on ngdocheck

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges, DoCheck } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
@@ -7,11 +7,14 @@ import { Router, ActivatedRoute } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, DoCheck {
   isAuthenticated = false;
   constructor(private auth: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  ngDoCheck():void {
     this.isAuthenticated = this.auth.loggedIn();
   }
 
